9121283455b627895f19b5c82dc0b96f7575b716
pablomanjarresneg <andresmanjarresneg@gmail.com>
2026-09-19 04:54:37 -0500

    add dark-first theme preference

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { applyStoredThemePreference } from '@/lib/theme'
 import { App } from './App'
 import './index.css'
+
+applyStoredThemePreference()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
